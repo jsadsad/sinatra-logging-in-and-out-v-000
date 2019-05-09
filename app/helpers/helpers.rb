d57d559d7dfc44aa457f8b89833d4173e1dccd64
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-09 22:11:23 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user
+  def self.current_user(session)
     @user = User.find_by(session[:user_id])
   end
   
-  def is_logged_in?
+  def self.is_logged_in?(session)
     !!session[:user_id] #return true if user_id is in the session hash
   end
 end
